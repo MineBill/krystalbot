ae550f5fca7306a497ef2924a515c1b597bff0de
MineBill <30367251+MineBill@users.noreply.github.com>
2018-09-17 10:54:18 +0300

    Commit

--- KrystalBot/KrystalBot/Program.cs
+++ KrystalBot/KrystalBot/Program.cs
@@ -13,11 +13,20 @@ namespace KrystalBot
         {
             BotSettings settings = new BotSettings();
             BotClient client = new BotClient(settings.BotName, settings.OAuthPass,settings.ChannelName,"!");
+            BotManager manager = new BotManager(client);
+
             client.Connect();
 
-            BotManager manager = new BotManager(client);
-            
-            Console.ReadKey();
+            bool readNext = true;
+            while (readNext)
+            {
+                string input = Console.ReadLine();
+
+                if (input.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    readNext = false;
+                }
+            }
         }
     }
 }
